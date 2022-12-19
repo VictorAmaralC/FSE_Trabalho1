2021d25dedad6f4297d135ab2cee668306c13618
Victor Amaral <victoramaralce@gmail.com>
2022-12-19 02:11:30 -0300

    Servidor distribuído finalizado, finalizando central

---
+++ Central/src/central.c
@@ -0,0 +1,165 @@
+#include "socket.h"
+#include "definitions.h"
+#include <errno.h>
+#include <signal.h>
+
+report *reports;
+int reportsLength = 0;
+int nViolations = 0;
+int nRedSign = 0;
+int mediaSpeed = 0;
+
+bool nightMode = false;
+bool emergencyMode = false;
+
+void updateReportsLength () {
+    reports = (report *) malloc((reportsLength + 1) * sizeof(report));
+    reportsLength++;
+}
+
+void addReport (int way, int speed, bool violation) {
+    updateReportsLength();
+    reports[reportsLength - 1].direction = way;
+    reports[reportsLength - 1].speed = speed;
+    reports[reportsLength - 1].violation = violation;
+}
+
+void handle_signal(int sig) {
+    // CTRL + \ /
+    if(sig == 3) {
+        nightMode = !nightMode;
+    }
+
+    // CTRL + Z
+    if(sig == 20) {
+        emergencyMode = !emergencyMode;
+    }
+}
+
+int main () {
+    // signal(SIGQUIT, handle_signal);
+    // signal(SIGTSTP, handle_signal);
+
+    bool opt = true;
+    int sockfd, connfd[4], len, i;
+    int max_sd, sd, new_socket, valread, addrlen, activity, max_conn = 4;
+    struct sockaddr_in address, cli;
+    fd_set readfds;
+    char buff[MAX];
+
+    for (i = 0; i < max_conn; i++) {  
+        connfd[i] = 0;  
+    } 
+
+    sockfd = configureSocket();
+    if( setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)) < 0 ) {  
+        perror("setsockopt");  
+        exit(EXIT_FAILURE);  
+    }  
+
+    bzero(&address, sizeof(address));
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = htonl(INADDR_ANY);
+    address.sin_port = htons(PORT);
+
+    bindSocket(sockfd, address);
+    listenSocket(sockfd);
+
+    addrlen = sizeof(address);
+
+    while(1) {
+        FD_ZERO(&readfds);
+        FD_SET(sockfd, &readfds);
+        max_sd = sockfd; 
+
+        for ( i = 0 ; i < max_conn ; i++) {  
+            sd = connfd[i];  
+
+            if(sd > 0)  
+                FD_SET(sd , &readfds);  
+                 
+            if(sd > max_sd)  
+                max_sd = sd;  
+        } 
+
+        activity = select( max_sd + 1 , &readfds , NULL , NULL , NULL);
+        if ((activity < 0) && (errno!=EINTR)) {  
+            printf("select error");  
+        }
+
+        if (FD_ISSET(sockfd, &readfds)) {
+            if ((new_socket = accept(sockfd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {  
+                perror("accept");  
+                exit(EXIT_FAILURE);  
+            }
+            for (i = 0; i < max_conn; i++) {  
+                if( connfd[i] == 0 ) {  
+                    connfd[i] = new_socket;  
+                    printf("Adding to list of sockets as %d\n" , i);          
+                    break;  
+                }
+            }  
+        }
+
+        for (i = 0; i < max_conn; i++) {  
+            sd = connfd[i];  
+            if (FD_ISSET(sd , &readfds)) {  
+                if ((valread = read( sd , buff, MAX)) == 0) {
+                    if(nightMode) {
+                        printf("Entrou aqui\n");
+                        buff[0] = 'n';
+                        buff[1] = 'i';
+                        buff[2] = 'g';
+                        buff[3] = 'h';
+                        buff[4] = 't';
+                        send(sd , buff , strlen(buff) , 0);
+                        bzero(buff, MAX);
+                    }
+
+                    if(emergencyMode) {
+                        buff[0] = 'e';
+                        buff[1] = 'm';
+                        buff[2] = 'e';
+                        buff[3] = 'r';
+                        buff[4] = 'g';
+                        send(sd , buff , strlen(buff) , 0);
+                        bzero(buff, MAX);
+                    }
+                    close( sd );  
+                    connfd[i] = 0;
+                } else {  
+                    int speed = buff[0];
+                    int direction = buff[1];
+                    bool violation = buff[2];
+
+                    addReport(direction, speed, violation);
+
+                    int carSpeedSum = 0;
+
+                    for(int i = 0; i < reportsLength; i++) {
+                        if(reports[i].violation && reports[i].speed < 60) {
+                            nRedSign++;
+                            nViolations++;
+                        }
+
+                        if(reports[i].speed > 60) {
+                            nViolations++;
+                        }
+
+                        carSpeedSum += reports[i].speed;
+                    }
+
+                    mediaSpeed = carSpeedSum / reportsLength;
+                    printf("Quantidade de Carros: %d\n", reportsLength);
+                    printf("Velocidade média: %d\n", mediaSpeed);
+                    printf("Ultrapassa sinal vermelho: %d\n", nRedSign);
+                    printf("Numero de infrações: %d\n", nViolations);
+                    bzero(buff, MAX);
+                }  
+            }  
+        } 
+    }
+
+    return 0;
+}
---
+++ Distribuido/src/main.c
@@ -0,0 +1,265 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <wiringPi.h>
+
+#include "socket.c"
+
+#define false 0
+#define true 1
+
+// Definicao Estrutura de sala
+struct Sala {
+    int pessoa;
+    int total;
+};
+
+Sala contadorSala;
+
+// Variaveis de definicao de sensores
+int Lampada1;
+int Lampada2;
+int ArCondicionado;
+int Projetor;
+int Alarme;
+int Presenca;
+int Fumaca;
+int Janela;
+int Porta;
+int Entrada;
+int Saida;
+int temperatura;
+
+// variaveis gerais
+int tempoEntrada = 0;
+int tempoSaida = 0;
+long ultimoMovimento = 0;
+long tempoPresenca = 0;
+char *aviso;
+bool alarmeTocando = false;
+bool luzesGerais = false;
+
+// Definições para Thread's e Socket
+pthread_t thread1;
+pthread_t thread2;
+int socket, connfd;
+
+// Chamada de funcoes
+void ativaPresenca(void);
+void ativaFumaca(void);
+void ativaJanela(void);
+void ativaPorta(void);
+void ativaEntrada(void);
+void ativaSaida(void);
+void mensagemAlerta(void);
+void atualizaTotalPessoas(void);
+
+// Funcao principal
+int int main(int argc, char const *argv[]) {
+    
+    pthread_create(&thread1, NULL, thread_func, NULL);
+
+    if(wiringPiSetup() == -1) return 1;
+
+    FILE* arquivoSala;
+
+    arquivoSala = fopen(argv[1], "r");
+    
+    if(arquivoSala) {
+        fscanf(arquivoSala, "%d", &Lampada1);
+        fscanf(arquivoSala, "%d", &Lampada2);
+        fscanf(arquivoSala, "%d", &ArCondicionado);
+        fscanf(arquivoSala, "%d", &Projetor);
+        fscanf(arquivoSala, "%d", &Alarme);
+        fscanf(arquivoSala, "%d", &Presenca);
+        fscanf(arquivoSala, "%d", &Fumaca);
+        fscanf(arquivoSala, "%d", &Janela);
+        fscanf(arquivoSala, "%d", &Porta);
+        fscanf(arquivoSala, "%d", &Entrada);
+        fscanf(arquivoSala, "%d", &Saida);
+        fscanf(arquivoSala, "%d", &temperatura);
+    } else {
+        printf("Arquivo de configuração de sala não encontrado/invalido\n");
+    }
+
+    fclose(arquivoSala);
+
+    pinMode(Lampada1,       OUTPUT);
+    pinMode(Lampada2,       OUTPUT);
+    pinMode(ArCondicionado, OUTPUT);
+    pinMode(Projetor,       OUTPUT);
+    pinMode(Alarme,         OUTPUT);
+    pinMode(Presenca,       INPUT);
+    pinMode(Fumaca,         INPUT);
+    pinMode(Porta,          INPUT);
+    pinMode(Janela,         INPUT);
+    pinMode(Entrada,        INPUT);
+    pinMode(Saida,          INPUT);
+
+    wiringPiISR(Presenca, INT_EDGE_RISING, &ativaPresenca);
+    wiringPiISR(Fumaca,   INT_EDGE_RISING, &ativaFumaca);
+    wiringPiISR(Porta,    INT_EDGE_RISING, &ativaPorta);
+    wiringPiISR(Janela,   INT_EDGE_RISING, &ativaJanela);
+    wiringPiISR(Entrada,  INT_EDGE_RISING, &ativaEntrada);
+    wiringPiISR(Saida,    INT_EDGE_RISING, &ativaSaida);
+
+    //Loop checagem de presença para desligar luzes gerais
+    while(1){
+        long checagem = millis();
+        
+        if ((checagem - tempoPresenca >= 1500) && luzesGerais == true){
+            digitalWrite(Lampada1, 0);
+            digitalWrite(Lampada2, 0);
+            luzesGerais = false;
+        }
+        tempoPresenca = checagem;
+        
+        delay(1000);
+    }
+
+    return 0;
+}
+
+void ativaPresenca(void) {
+    mensagemAlerta(Presenca);
+}
+
+void ativaFumaca(void) {
+    mensagemAlerta(Fumaca);
+}
+
+void ativaJanela(void) {
+    mensagemAlerta(Janela);
+}
+
+void ativaPorta(void) {
+    mensagemAlerta(Porta);
+}
+
+void ativaEntrada(void) {
+    long tempoEntrada = millis();
+    
+    if(tempoEntrada - ultimoMovimento > 200){
+        atualizaTotalPessoas(Entrada);
+    }
+    
+    ultimoMovimento = tempoEntrada;
+}
+
+void ativaSaida(void) {
+    long tempoSaida = millis();
+
+    if(tempoSaida - ultimoMovimento > 200){
+        atualizaTotalPessoas(Saida);
+    }
+    
+    ultimoMovimento = tempoSaida;
+}
+
+void enviaAvisoCentral(char *aviso){
+    write(sockfd, aviso, sizeof(aviso));
+    bzero(aviso, sizeof(aviso));
+}
+
+void mensagemAlerta(int sensor){
+    switch(sensor) {
+      case Presenca :
+        aviso = "Sensor de presenca ativado!\n";
+
+        if(alarmeTocando == true){
+            if(digitalRead(Alarme) == 0)
+                digitalWrite(Alarme, 1);
+            aviso += " Alarme ativado!";
+        } else if (alarmeTocando == false){
+            digitalWrite(Lampada1, 1);
+            digitalWrite(Lampada2, 1);
+            tempoPresenca = millis();
+            luzesGerais = true;
+            aviso += " Luzes acesas!";
+        }
+
+        enviaAvisoCentral(aviso);
+        break;
+      case Fumaca :
+        alarmeTocando = true;
+        
+        if(digitalRead(Alarme) == 0)
+            digitalWrite(Alarme, 1);
+        
+        aviso = "Sensor de fumaca ativado!\n";
+        enviaAvisoCentral(aviso);
+        break;
+      case Janela :
+        aviso = "Sensor de janela ativado!\n";
+        enviaAvisoCentral(aviso);
+        break;
+      case Porta :
+        aviso = "Sensor de porta ativado!\n";
+        enviaAvisoCentral(aviso);
+        break;
+      default :
+        aviso = "Nenhum sensor ativado...\n";
+        enviaAvisoCentral(aviso);
+   }
+}
+
+void atualizaTotalPessoas(int alteracaoMovimento){
+    if(alteracaoMovimento == Entrada){
+        contadorSala.pessoa++;
+        contadorSala.total++;
+    } else if(alteracaoMovimento == Saida){
+        rcontadorSala.pessoa--;
+        contadorSala.total--;
+    }
+}
+
+void alteraEstadoSensor(int pino){
+    if (digitalRead(pino) == 1){
+        digitalWrite(pino, 0);
+    }
+    else{
+        digitalWrite(pino, 1);
+    }
+}
+
+void *thread_func (void *arg) {
+    struct sockaddr_in endServidor;
+   
+    socket = setupSocket();
+    bzero(&endServidor, sizeof(endServidor));
+   
+    endServidor.sin_family = AF_INET;
+    endServidor.sin_addr.s_addr = inet_addr("164.41.98.16");
+    endServidor.sin_port = htons(13508);
+   
+    connectSocketServer(socket, endServidor);
+   
+    //pthread_create(&threadB, NULL, thread_funcB, (void*)socket);
+    func(socket);   
+    close(socket);
+}
+
+/*void *thread_funcB (void *arg) {
+    char buff[MAX];
+    int n;
+    int socket = (int) arg;
+
+    while(1) {
+        read(socket, buff, MAX);
+        printf("%s\n", buff);
+    }
+}*/
+
+void func(int socket) {
+    char buffer[MAX];
+    int n;  
+
+    while(1) {
+        bzero(buffer, sizeof(buffer));
+        buff[0] = contadorSala[n].pessoa;
+        buff[1] = contadorSala[n].Total;
+        write(socket, buffer, sizeof(buffer));
+        bzero(buffer, sizeof(buffer));
+        delay(2000); //tempo de delay baseado no sensor de temperatura  
+    }
+}
